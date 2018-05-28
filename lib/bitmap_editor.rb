54044be0a30e170590144e4f821f28017534e0b1
Alexey Artamonov <i@useruby.com>
2018-05-28 21:40:33 +0200

    Copy functionality from master for handling the state when file is missing

--- lib/bitmap_editor.rb
+++ lib/bitmap_editor.rb
@@ -1,13 +1,19 @@
 require 'bitmap_editor/interpreter'
 
 class BitmapEditor
+  PROVIDE_CORRECT_FILE = 'please provide correct file'.freeze
+
   attr_reader :script
 
   def run(file_name)
+    return puts(PROVIDE_CORRECT_FILE) if file_name.nil?
+    
     @script = File.read(file_name)
 
     Interpreter.new(script).execute
-  
+ 
+  rescue SystemCallError, IOError
+    puts(PROVIDE_CORRECT_FILE)
   rescue Runtime::Error => error
     puts(error.message)
   end
